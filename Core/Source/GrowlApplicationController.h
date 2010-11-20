83240694ec4420527de672f6dee1db15d6a7231a
Rudy Richter <Rudy Richter@localhost>
2010-11-20 14:10:59 -0500

    GrowlMail: clang says that we should drain instead of release

---
+++ Core/Source/GrowlApplicationController.h
@@ -0,0 +1,85 @@
+//
+//  GrowlApplicationController.h
+//  Growl
+//
+//  Created by Karl Adam on Thu Apr 22 2004.
+//  Renamed from GrowlController by Mac-arena the Bored Zo on 2005-06-28.
+//  Copyright 2004-2006 The Growl Project. All rights reserved.
+//
+// This file is under the BSD License, refer to License.txt for details
+
+#import <Foundation/Foundation.h>
+#import "GrowlApplicationBridge.h"
+#import "GrowlAbstractSingletonObject.h"
+
+@class GrowlNotificationCenter, GrowlTicketController, GrowlNotificationHistoryWindow;
+
+typedef enum {
+	GrowlNotificationResultPosted,
+	GrowlNotificationResultNotRegistered,
+	GrowlNotificationResultDisabled
+} GrowlNotificationResult;
+
+@interface GrowlApplicationController : GrowlAbstractSingletonObject <GrowlApplicationBridgeDelegate> {
+	GrowlTicketController		*ticketController;
+
+	// local GrowlNotificationCenter
+	NSConnection				*growlNotificationCenterConnection;
+	GrowlNotificationCenter		*growlNotificationCenter;
+
+	GrowlDisplayPlugin			*defaultDisplayPlugin;
+
+	BOOL						growlIsEnabled;
+	BOOL						growlFinishedLaunching;
+	BOOL						quitAfterOpen;
+	BOOL						enableForward;
+	NSArray						*destinations;
+
+	NSDictionary				*versionInfo;
+	NSImage						*growlIcon;
+	NSData						*growlIconData;
+	
+	CFRunLoopTimerRef			updateTimer;
+	
+	NSThread					*mainThread;
+   
+   NSDate               *awayDate;
+   NSDate               *returnDate;
+   GrowlNotificationHistoryWindow *historyWindow;
+}
+
++ (GrowlApplicationController *) sharedController;
+
+- (BOOL) application:(NSApplication *)theApplication openFile:(NSString *)filename;
+
++ (NSString *) growlVersion;
+
+- (GrowlNotificationResult) dispatchNotificationWithDictionary:(NSDictionary *)dict;
+- (BOOL) registerApplicationWithDictionary:(NSDictionary *)userInfo;
+- (void)growlNotificationDict:(NSDictionary *)growlNotificationDict didCloseViaNotificationClick:(BOOL)viaClick onLocalMachine:(BOOL)wasLocal;
+
+- (NSDictionary *) versionDictionary;
+- (NSString *) stringWithVersionDictionary:(NSDictionary *)d;
+
+- (void) preferencesChanged:(NSNotification *) note;
+
+- (void) shutdown:(NSNotification *)note;
+- (void) replyToPing:(NSNotification *)note;
+
+#pragma mark Accessors
+
+//To be used by the GAB pathway if it can't register its connection (which means that there's already a GHA running).
+- (BOOL) quitAfterOpen;
+- (void) setQuitAfterOpen:(BOOL)flag;
+
+- (NSThread *)mainThread;
+
+#pragma mark Sparkle
+
+- (void) timedCheckForUpdates:(NSNotification*)note;
+- (void) growlNotificationWasClicked:(id)clickContext;
+- (void) growlNotificationTimedOut:(id)clickContext;
+- (void) launchSparkleHelper;
+- (void) growlSparkleHelperFoundUpdate:(NSNotification*)note;
+	
+@end
---
+++ Developer Tools/GrowlMailUUIDPatcher/source/GrowlMailUUIDPatcherAppDelegate.h
@@ -0,0 +1,42 @@
+//
+//  GrowlMailUUIDPatcherAppDelegate.h
+//  GrowlMailUUIDPatcher
+//
+//  Created by Rudy Richter on 7/10/10.
+//  Copyright 2010 Beware Reactor. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface GrowlMailUUIDPatcherAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+	NSTextField *status;
+	NSTextField *needsUpdate;
+	NSButton *updateButton;
+	
+	NSString *mailAppUUID;
+	NSString *messageFrameworkUUID;
+	
+	NSArray *paths;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+@property (assign) IBOutlet NSTextField *status;
+@property (assign) IBOutlet NSTextField *needsUpdate;
+@property (assign) IBOutlet NSButton *updateButton;
+
+@property (retain) NSString *mailAppUUID;
+@property (retain) NSString *messageFrameworkUUID;
+
+@property (retain) NSArray *paths;
+
+- (void)getUUIDs;
+- (NSArray*)growlMailPaths;
+- (void)verify;
+- (BOOL)plistAtPathPromisesCompatibilityWithCurrentMailAndMessageFramework:(NSString*)path;
+- (IBAction)updatePlist:(id)sender;
+- (BOOL)mailIsRunning;
+- (void)relaunchMail;
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context;
+
+@end

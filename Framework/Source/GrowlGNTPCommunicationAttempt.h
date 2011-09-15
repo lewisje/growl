ef75c2bac4a91015ac9da6c2bc62f6076ee04045
Rachel Blackman <ceruleansparks@gmail.com>
2011-09-14 19:56:43 -0700

    I have been eaten by work.  Cleaning up my sandboxing/XPC code and committing to pass off to Falk.

---
+++ Framework/Source/GrowlGNTPCommunicationAttempt.h
@@ -0,0 +1,39 @@
+//
+//  GrowlGNTPCommunicationAttempt.h
+//  Growl
+//
+//  Created by Peter Hosey on 2011-07-14.
+//  Copyright 2011 The Growl Project. All rights reserved.
+//
+
+#import "GrowlCommunicationAttempt.h"
+
+@class GrowlGNTPOutgoingPacket;
+@class GCDAsyncSocket;
+
+@interface GrowlGNTPCommunicationAttempt : GrowlCommunicationAttempt
+{
+	GrowlGNTPOutgoingPacket *packet;
+	GCDAsyncSocket *socket;
+	NSString *responseParseErrorString, *bogusResponse;
+	NSString *callbackType;
+	NSMutableArray *callbackHeaderItems;
+	BOOL attemptSucceeded;
+   int responseReadType;
+   
+   NSString *host;
+   NSString *password;
+}
+@property (nonatomic, retain) NSString *host;
+@property (nonatomic, retain) NSString *password;
+
+//Lazily constructs the packet for self.dictionary.
+- (GrowlGNTPOutgoingPacket *) packet;
+
+//Returns NO. Subclasses may overrido to conditionally or unconditionally return YES.
+- (BOOL) expectsCallback;
+
+- (void)parseError;
+- (void)parseFeedback;
+
+@end

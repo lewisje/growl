47cfe2ce200d417b25f74118252a54b0c19a3b15
Daniel Siemer <daniel@growl.info>
2011-09-16 10:18:19 -0500

    Rewrote XPC detection, use xpc function = NULL for system version detection still, but switch to asking the file system if the bundle is there.  xpc_connection_create creates a connection regardless of whether there is a bundle there or not, and only fails after you set a handler on it and try to resume it. Improved error handling logic, still needs work on the XPC side

---
+++ XPC/GNTP Client/GrowlNotifier.m
@@ -0,0 +1,88 @@
+//
+//  GrowlNotifier.m
+//  Growl
+//
+//  Created by Daniel Siemer on 9/15/11.
+//  Copyright 2011 The Growl Project. All rights reserved.
+//
+
+#import "GrowlNotifier.h"
+#import "GrowlDefines.h"
+#import "GrowlGNTPCommunicationAttempt.h"
+#import "GrowlGNTPRegistrationAttempt.h"
+#import "GrowlGNTPNotificationAttempt.h"
+
+#import "NSObject+XPCHelpers.h"
+
+@implementation GrowlNotifier
+
+@synthesize currentAttempts;
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void) sendCommunicationAttempt:(GrowlCommunicationAttempt *)attempt 
+{
+   [currentAttempts addObject:attempt];
+   [attempt begin];
+}
+
+-(void)sendXPCMessage:(id)nsMessage connection:(xpc_connection_t)connection
+{
+   xpc_object_t message = [(NSObject*)nsMessage newXPCObject];
+   xpc_connection_send_message(connection, message);
+   xpc_release(message);
+}
+
+- (void) sendXPCFeedback:(GrowlCommunicationAttempt *)attempt context:(id)context clicked:(BOOL)clicked
+{
+   NSMutableDictionary *response = [NSMutableDictionary dictionary];
+   [response setValue:@"feedback" forKey:@"GrowlActionType"];
+   
+   [response setValue:context forKey:@"Context"];
+   [response setValue:[NSNumber numberWithBool:clicked] forKey:@"Clicked"];
+   
+   [self sendXPCMessage:response connection:[(GrowlGNTPCommunicationAttempt*)attempt connection]];
+}
+
+- (void) attemptDidSucceed:(GrowlCommunicationAttempt *)attempt{
+   NSMutableDictionary *response = [NSMutableDictionary dictionary];
+   [response setValue:[NSNumber numberWithBool:YES] forKey:@"Success"];
+   
+   if([attempt isKindOfClass:[GrowlGNTPRegistrationAttempt class]]){
+      [response setValue:@"registration" forKey:@"GrowlActionType"];
+   }else{
+      //We should only have GNTP Registration and Notification
+      [response setValue:@"notification" forKey:@"GrowlActionType"];
+   }
+   
+   [self sendXPCMessage:response connection:[(GrowlGNTPCommunicationAttempt*)attempt connection]];
+}
+- (void) attemptDidFail:(GrowlCommunicationAttempt *)attempt{
+   [currentAttempts removeObject:attempt];
+}
+- (void) finishedWithAttempt:(GrowlCommunicationAttempt *)attempt{
+   [currentAttempts removeObject:attempt];
+}
+- (void) queueAndReregister:(GrowlCommunicationAttempt *)attempt{
+   //we will have to ask our host app for the reg dict again via XPC
+   NSMutableDictionary *response = [NSMutableDictionary dictionary];
+   [response setValue:@"reregister" forKey:@"GrowlActionType"];
+   
+   [self sendXPCMessage:response connection:[(GrowlGNTPCommunicationAttempt*)attempt connection]];
+}
+- (void) notificationClicked:(GrowlCommunicationAttempt *)attempt context:(id)context{
+   [self sendXPCFeedback:attempt context:context clicked:YES];
+}
+- (void) notificationTimedOut:(GrowlCommunicationAttempt *)attempt context:(id)context{
+   [self sendXPCFeedback:attempt context:context clicked:NO];
+}
+
+@end

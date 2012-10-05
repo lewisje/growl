5c18f024acc7eccc81a1ff3cd0ff7139dd75ba9c
Daniel Siemer <daniel@growl.info>
2012-10-05 11:23:02 -0500

    Let the work on 2.1 begin, basic proof of concept for rules powered by AppleScript called by NSUserAppleScriptTask.  Initial proof of concept for passing a set of values to applescript, and returning the actions to take.  Does not actually adjust behavior yet.

---
+++ GrowlLauncher/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GrowlLauncher
+//
+//  Created by Daniel Siemer on 1/26/12.
+//  Copyright (c) 2012 The Growl Project, LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
